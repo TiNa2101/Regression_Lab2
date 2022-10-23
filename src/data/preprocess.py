6984777d3d6f61ef2a497c48878e2049e9d3a74a
Nina Titova <nititova@edu.hse.ru>
2022-10-23 22:55:15 +0300

    prepro apd

--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -1,49 +1,48 @@
 import pandas as pd
 import numpy as np
 import src.config as cfg
-from src.config import TARGET_COLS
 
 
-def drop_unnecesary_id(df: pd.DataFrame) -> pd.DataFrame:
-    if 'ID_y' in df.columns:
-        df = df.drop('ID_y', axis=1)
-    return df
-
-
-def fill_sex(df: pd.DataFrame) -> pd.DataFrame:
-    most_freq = df[cfg.SEX_COL].value_counts().index[0]
-    df[cfg.SEX_COL] = df[cfg.SEX_COL].fillna(most_freq)
+def fill_fields(df: pd.DataFrame) -> pd.DataFrame:
+    df['FireplaceQu'] = df['FireplaceQu'].fillna('No Fireplace')
+    df['BsmtQual'] = df['BsmtQual'].fillna('No Basement')
+    df['BsmtCond'] = df['BsmtCond'].fillna('No Basement')
+    df['BsmtExposure'] = df['BsmtExposure'].fillna('No Basement')
+    df['BsmtFinType1'] = df['BsmtFinType1'].fillna('No Basement')
+    df['BsmtFinType2'] = df['BsmtFinType2'].fillna('No Basement')
+    df['MasVnrArea'] = df['MasVnrArea'].fillna(0)
+    df['MasVnrType'] = df['MasVnrType'].fillna('None')
+    df['Alley'] = df['Alley'].fillna('No alley access')
+    most_freq = df['LotFrontage'].value_counts().index[0]
+    df['LotFrontage'] = df['LotFrontage'].fillna(most_freq)
+    df['GarageType'] = df['GarageType'].fillna('No Garage')
+    df['GarageFinish'] = df['GarageFinish'].fillna('No Garage')
+    df['GarageQual'] = df['GarageQual'].fillna('No Garage')
+    df['GarageCond'] = df['GarageCond'].fillna('No Garage')
+    df['PoolQC'] = df['PoolQC'].fillna('No Pool')
+    df['Fence'] = df['Fence'].fillna('No Fence')
+    df['MiscFeature'] = df['MiscFeature'].fillna('None')
     return df
 
 
 def cast_types(df: pd.DataFrame) -> pd.DataFrame:
+    df[cfg.ID_COL] = df[cfg.ID_COL].astype(np.int8)
     df[cfg.CAT_COLS] = df[cfg.CAT_COLS].astype('category')
-
-    ohe_int_cols = df[cfg.OHE_COLS].select_dtypes('number').columns
-    df[ohe_int_cols] = df[ohe_int_cols].astype(np.int8)
-
     df[cfg.REAL_COLS] = df[cfg.REAL_COLS].astype(np.float32)
     return df
 
 
-def set_idx(df: pd.DataFrame, idx_col: str) -> pd.DataFrame:
-    df = df.set_index(idx_col)
-    return df
-    
-
 def preprocess_data(df: pd.DataFrame) -> pd.DataFrame:
-    df = set_idx(df, cfg.ID_COL)
-    df = drop_unnecesary_id(df)
-    df = fill_sex(df)
+    df = fill_fields(df)
     df = cast_types(df)
     return df
 
 
 def preprocess_target(df: pd.DataFrame) -> pd.DataFrame:
-    df[cfg.TARGET_COLS] = df[cfg.TARGET_COLS].astype(np.int8)
+    df[cfg.TARGET_COL] = df[cfg.TARGET_COL].astype(np.int32)
     return df
 
 
 def extract_target(df: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame]:
-    df, target = df.drop(cfg.TARGET_COLS, axis=1), df[TARGET_COLS].copy()
+    df, target = df.drop(cfg.TARGET_COL, axis=1), df[cfg.TARGET_COL].copy()
     return df, target
